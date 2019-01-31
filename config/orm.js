7f05eed3495e4e90bfd4f5d376940fcfed8c945d
JMerritt94 <Jay.P.Merritt@gmail.com>
2019-01-31 16:34:45 -0500

    bugfix

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,31 @@
-const connection = require("./connection")
+const connection = require("./connection");
 
-const orm={
-    getBurgers: function(cb){
-        
-        connection.query("SELECT * FROM burgers;",function(err,result){
-        
-        if(err) throw err;
-        cb(result)}
-   
-        )},
+const orm = {
+  getBurgers: function(cb) {
+    connection.query("SELECT * FROM burgers;", function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
 
-    eat: function(state,cb){
-        connection.query('UPDATE burgers SET devoured=true WHERE id='+state+';',cb)
-    },
+  eat: function(state, cb) {
+    connection.query(
+      "UPDATE burgers SET devoured=true WHERE id=" + state + ";",
+      cb
+    );
+  },
 
-    reviveBurger: function(cb){
-connection.query('UPDATE burgers SET devoured = NOT Devoured',cb )},
+  reviveBurger: function(cb) {
+    connection.query("UPDATE burgers SET devoured = FALSE", cb);
+  },
 
+  create: function(val, cb) {
+    connection.query(
+      "INSERT INTO burgers (`burger_name`,`devoured`) VALUES ('" + val + "',FALSE);",
+      cb
+    );
+  }                       
+};
 
-create: function(val,cb){
-    connection.query('INSERT INTO burgers (burger_name) Values' +val+");", cb
-      
-    )
-}
-}
-
-module.exports=orm;
-// in es6 this would be 
+module.exports = orm;
+// in es6 this would be
